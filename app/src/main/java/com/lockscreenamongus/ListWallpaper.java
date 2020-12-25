534b97dba94d350921b4ee6ef488cff675c735c9
unknown <lonelyheart1402@gmail.com>
2020-12-25 01:52:02 -0800

    update3

--- app/src/main/java/com/lockscreenamongus/ListWallpaper.java
+++ app/src/main/java/com/lockscreenamongus/ListWallpaper.java
@@ -8,11 +8,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AudienceNetworkAds;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.lockscreenamongus.R;
 import com.lockscreenamongus.utils.RecyclerItemClickListener;
 import com.lockscreenamongus.utils.Wallpaper;
@@ -20,12 +30,14 @@ import com.lockscreenamongus.utils.wallpaperAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ListWallpaper extends AppCompatActivity {
     private List<Wallpaper> wallpapersList = new ArrayList<>();
     private RecyclerView recyclerView;
     private wallpaperAdapter mAdapter;
     private ImageView back;
+    private com.facebook.ads.AdView adView;
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +60,7 @@ public class ListWallpaper extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(ListWallpaper.this, 2, GridLayout.VERTICAL, false));
         recyclerView.setAdapter(mAdapter);
         prepareMovieData();
-
+        loadBannerFB();
         recyclerView.addOnItemTouchListener(
                 new RecyclerItemClickListener(ListWallpaper.this, recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
                     @Override public void onItemClick(View view, int position) {
@@ -143,5 +155,99 @@ public class ListWallpaper extends AppCompatActivity {
         wallpapersList.add(wallpaper);
 
         mAdapter.notifyDataSetChanged();
+    }
+    public void loadBannerGG() {
+        final View adContainer = findViewById(R.id.adMobView);
+        final AdView mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.BANNER);
+        mAdView.setAdUnitId(MainActivity.BANNER_ID);
+        ((LinearLayout) adContainer).addView(mAdView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                adContainer.setBackgroundColor(Color.GRAY);
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
+
+        Random r = new Random();
+        int ads = r.nextInt(100);
+
+        if (ads >= MainActivity.PERCENT_SHOW_BANNER_AD) {
+            mAdView.destroy();
+            mAdView.setVisibility(View.GONE);
+        }
+    }
+
+    public void loadBannerFB() {
+        final View adContainer = findViewById(R.id.adMobView);
+        AudienceNetworkAds.initialize(this);
+        adView = new com.facebook.ads.AdView(this, MainActivity.BANNER_ID_FB, com.facebook.ads.AdSize.BANNER_HEIGHT_50);
+        ((LinearLayout) adContainer).addView(adView);
+
+// Request an ad
+        com.facebook.ads.AdListener adListener = new com.facebook.ads.AdListener() {
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                loadBannerGG();
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Ad loaded callback
+                adContainer.setBackgroundColor(Color.GRAY);
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+            }
+        };
+
+        // Request an ad
+        adView.loadAd(adView.buildLoadAdConfig().withAdListener(adListener).build());
+
+        Random r = new Random();
+        int ads = r.nextInt(100);
+
+        if (ads >= MainActivity.PERCENT_SHOW_BANNER_AD) {
+            adView.destroy();
+            adView.setVisibility(View.GONE);
+        }
+
     }
 }
